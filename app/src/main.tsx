69d317a872962f93da086540b94e6b40cb4bd0a7
red <144563381+Redurbabat@users.noreply.github.com>
2026-09-12 23:13:28 +0200

    feat: load playful seed design pack

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import './custom/registerFunSeeds'
 import './styles/globals.css'
 import './styles/components.css'
 import './styles/style-packs.css'
