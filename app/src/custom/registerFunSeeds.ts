a57209254817d0ce1cf7104ae81986401dd24a68
red <144563381+Redurbabat@users.noreply.github.com>
2026-09-13 00:22:22 +0200

    Register user snippet seed designs

--- app/src/custom/registerFunSeeds.ts
+++ app/src/custom/registerFunSeeds.ts
@@ -1,10 +1,11 @@
 import { seedDesigns } from './seedDesigns'
 import { funSeedDesigns } from './funSeedDesigns'
 import { rotatingCardSeedDesigns } from './rotatingCardSeeds'
+import { userSnippetSeedDesigns } from './userSnippetSeeds'
 
 const existing = new Set(seedDesigns.map((design) => design.id))
 
-for (const design of [...funSeedDesigns, ...rotatingCardSeedDesigns]) {
+for (const design of [...funSeedDesigns, ...rotatingCardSeedDesigns, ...userSnippetSeedDesigns]) {
   if (!existing.has(design.id)) {
     seedDesigns.push(design)
     existing.add(design.id)
