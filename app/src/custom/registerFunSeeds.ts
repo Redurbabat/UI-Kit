1c3ae3e133bc0470e1c334d3feebeb039ec56efb
red <144563381+Redurbabat@users.noreply.github.com>
2026-09-13 00:26:51 +0200

    feat: register flowing KPI card row

--- app/src/custom/registerFunSeeds.ts
+++ app/src/custom/registerFunSeeds.ts
@@ -2,10 +2,11 @@ import { seedDesigns } from './seedDesigns'
 import { funSeedDesigns } from './funSeedDesigns'
 import { rotatingCardSeedDesigns } from './rotatingCardSeeds'
 import { userSnippetSeedDesigns } from './userSnippetSeeds'
+import { kpiFlowSeedDesigns } from './kpiFlowSeeds'
 
 const existing = new Set(seedDesigns.map((design) => design.id))
 
-for (const design of [...funSeedDesigns, ...rotatingCardSeedDesigns, ...userSnippetSeedDesigns]) {
+for (const design of [...funSeedDesigns, ...rotatingCardSeedDesigns, ...userSnippetSeedDesigns, ...kpiFlowSeedDesigns]) {
   if (!existing.has(design.id)) {
     seedDesigns.push(design)
     existing.add(design.id)
